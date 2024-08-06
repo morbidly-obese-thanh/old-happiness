0a1b5fbadcdbf28a0d91b6dfaf8c6025a8bc5e1e
morbidly-obese-thanh <morbidly.obese.thanh@outlook.com>
2024-08-06 11:49:20 +0200

    * gas/conf*/tc-ha*.c: Add comment explaining them.

--- Toolchain/Tarballs/binutils-2.43/gas/config/tc-happiness.c
+++ Toolchain/Tarballs/binutils-2.43/gas/config/tc-happiness.c
@@ -27,44 +27,59 @@ struct option md_longopts[] = {};
 
 size_t md_longopts_size = sizeof(md_longopts);
 
+/* Comment characters and separators. */
 const char comment_chars[] = "";
 const char line_comment_chars[] = "//";
 const char line_separator_chars[] = ";";
 const char EXP_CHARS[] = "";
 const char FLT_CHARS[] = "";
 
+/* No pseudo ops yet. */
 const pseudo_typeS md_pseudo_table[] = {{(char *)0, (void (*)(int))0, 0}};
 
+/* Can't parse if there are no opts. */
 int md_parse_option(int c, const char *arg) { return 0; }
 
+/* Command line usage. */
 void md_show_usage(FILE *stream) {
   fprintf(stream, "\nHappiness options: none available yet\n");
 }
 
+/* Custom initializer hook, called once per assembler invocation. */
 void md_begin(void) { return; }
 
+/* Main assembler hook, called once for each "instruction string." */
 void md_assemble(char *insn_str) { return; }
 
+/* This gets invoked in case 'md_parse_name' has failed. */
 symbolS *md_undefined_symbol(char *name) { return 0; }
 
+/* Again, no floating point expressions available. */
 const char *md_atof(int type, char *lit, int *size) { return 0; }
 
+/* Can't round up sections yet. */
 valueT md_section_align(asection *seg, valueT val) { return 0; }
 
+/* Also, generally no support for frag conversion. */
 void md_convert_frag(bfd *abfd, asection *seg, fragS *fragp) {
   as_fatal(_("unexpected call"));
   return;
 }
 
+/* Not possible, instead convert to relocs and write them. */
 void md_apply_fix(fixS *fixp, valueT *val, segT seg) { return; }
 
+/* If not applied/resolved, turn a fixup into a relocation entry. */
 arelent *tc_gen_reloc(asection *seg, fixS *fixp) { return 0; }
 
+/* No pc-relative relocations yet, might provide one later for experiments. */
 long md_pcrel_from(fixS *fixp) {
   as_fatal(_("unexpected call"));
   return 0;
 }
 
+/* All instructions and immediates are fixed in size, therefore no relaxation
+ * required. */
 int md_estimate_size_before_relax(fragS *fragp, asection *seg) {
   as_fatal(_("unexpected call"));
   return 0;
